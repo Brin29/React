50441286cd0541b1a68163d6b03a3dd6d6d86774
Brin29 <breinerstivenparracortes50@gmail.com>
2024-02-05 10:59:31 -0500

    Revert "Avanzes"
    This reverts commit 1ae861f84f8d8674bcc6d8f7fcd5c56598b0a111.
    
    Arreglando la cagada

--- 04-prueba-tecnica/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import {App} from './src/App.jsx'
-
-// Como es un archivo js vite no transpila el codigo debe ser un archivo jsx
-const root = createRoot(document.getElementById('app'))
-root.render(
-  <App firstWord={"ciao"}/>
-)
